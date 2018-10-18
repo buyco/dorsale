07cd97db7c94b8de4bb37881d552fb26192202ab
Jose GARIBOTTI <jose@buyco.co>
2018-10-18 15:22:44 +0200

    Fixed tests

--- app/helpers/dorsale/all_helpers.rb
+++ app/helpers/dorsale/all_helpers.rb
@@ -4,7 +4,6 @@ module Dorsale::AllHelpers
 
   include ::Dorsale::ContextHelper
 
-  include ::Dorsale::CommentsHelper
   include ::Dorsale::Alexandrie::AttachmentsHelper
   include ::Dorsale::UsersHelper
 
